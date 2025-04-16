f35f5652ecec1308eeab5dba99e94c20a1a2dcfe
Makaronnz <guneysen@gmail.com>
2025-04-16 04:07:00 +0200

    Calculator added

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from calculator import add, subtract, multiply, divide
+
 def main():
     print("PyCharm and Git integration")
+    print(f"5 + 3 = {add(5, 3)}")
+    print(f"5 - 3 = {subtract(5, 3)}")
+    print(f"5 * 3 = {multiply(5, 3)}")
+    print(f"5 / 2 = {divide(5, 2)}")
 
 if __name__ == "__main__":
     main()
